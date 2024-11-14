7bda302d0a2821c157f4b0c1b688f81df939c0ea
sleung124 <samuelleung124@gmail.com>
2024-11-14 10:51:13 -0800

    added jobscript sample, updated readme

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
-### Sockeye-Rstudio
+# Sockeye-Rstudio
 
-Toy workspace to show how to work with Rstudio and sockeye
+- Toy workspace to show how to work with Rstudio and sockeye
+
+## What you will need:
+- dockerhub account
+- docker image you want to use
+- access to sockeye
+
+## Steps for running Rstudio on Sockeye
+1. Go to your project directory with:
+
+        cd $PROJECT_PATH
+
+2. Make a new folder `images` to store the `.sif` images needed to run rstudio, and go into it with the following commands:
+
+        mkdir {name-of-folder}
+        cd {name-of-folder}
+
+3. Build the dockerhub image as a SIF file:
+
+        module load apptainer
+        apptainer build {name-of-sif-image}.sif docker://{docker-image-path}
+
+4. Go to your scratch directory, where your project should be in 
+
+        cd $SCRATCH_PATH/{your-project-path}
+
+5. Within the folder with your jobscript, run
+
+        sbatch {your-job-script}.sh
+
+    Wait a couple seconds, then view the contents of connection_jobid.txt (in your job dir, e.g. "connection_1234.txt") for SSH tunnel and connection instructions.
+
+    When you're done, delete your job with 
+
+        scancel {jobid}
+        rm {jobid}.txt
+
+
+        
+
+## Making your own docker image
+
+### REMINDER:
+- if using `make` commands, be sure to edit the Makefile to your docker account and for your project
+
+1. Go into `code/install_r_packages.r` and specify what packages you want to install
+2. Build docker image by running `Make build` in the terminal of your project directory. If `make` command is not installed, you can also run:
+
+        docker build -t {your-docker-acct}/{docker-image-name} .
+
+3. Push docker image to dockerhub with either `Make push` or the following:
+
+        docker push sleung124/rstudio-sockeye-tutorial
---
+++ job_scripts/sample_jobscript.sh
@@ -0,0 +1,55 @@
+module purge
+module load gcc/7.5.0 apptainer/1.3.1
+
+# Set RANDFILE location to writeable dir
+export RANDFILE=$TMPDIR/.rnd
+
+# Generate a unique password for RStudio Server
+export APPTAINERENV_PASSWORD=$(openssl rand -base64 15)
+
+# Find a unique port for RStudio Server to listen on
+readonly PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
+
+# Set per-job location for the rserver secure cookie
+export SECURE_COOKIE=$TMPDIR/secure-cookie-key
+
+# Print connection details to file
+cat > connection_${PBS_JOBID}.txt <<END
+
+1. Create an SSH tunnel to RStudio Server from your local workstation using the following command:
+
+ssh -N -L 8787:${HOSTNAME}:${PORT} ${USER}@sockeye.arc.ubc.ca
+
+2. Point your web browser to http://localhost:8787
+
+3. Login to RStudio Server using the following credentials:
+
+Username: ${USER}
+Password: ${APPTAINERENV_PASSWORD}
+
+When done using RStudio Server, terminate the job by:
+
+1. Sign out of RStudio (Left of the "power" button in the top right corner of the RStudio window)
+2. Issue the following command on the login node:
+
+scancel ${PBS_JOBID}
+
+END
+
+# CHANGE THESE FOR YOUR PROJECT
+# IMAGEFILE is the name of your .sif file 
+IMAGEFILE=rstudio-sockeye-tutorial.sif
+# PROJDIR is the name of your project directory
+PROJDIR=Rstudio-Sockeye
+
+# HOME should be where your project directory is at
+HOME=$SCRATCH_PATH/$PROJDIR
+
+# IMAGE should be where your image directory is at
+IMAGE=$PROJECT_PATH/rstudio/images/$IMAGEFILE
+
+# Execute the rserver within the rocker/rstudio container
+apptainer exec --bind $TMPDIR:/var/run/ --bind $TMPDIR:/var/lib/rstudio-server \
+                --home $HOME $IMAGE \
+                rserver --auth-none=0 --auth-pam-helper-path=pam-helper --secure-cookie-key-file \
+                ${SECURE_COOKIE} --www-port ${PORT} --server-user ${USER}
